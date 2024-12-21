a80407f1310ec7fbdf7f8c1c8b36688c4cc522f2
veasnachroeun <115873548+veasnachroeun@users.noreply.github.com>
2024-12-21 18:35:58 +0700

    update

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -18,9 +18,127 @@ if(pageName ==="Starter.html"){
 if(pageName ==="Lesson1.html"){
     document.querySelector(".divaword").classList.add("activelink");
 }
+if(pageName ==="Lesson2.html"){
+    document.querySelector(".barword").classList.add("activelink");
+}
+if(pageName ==="Lesson2.html"){
+    document.querySelector(".dviaword2").classList.add("activelink");
+}
+if(pageName ==="Lesson3.html"){
+    document.querySelector(".barword").classList.add("activelink");
+}
+if(pageName ==="Lesson3.html"){
+    document.querySelector(".dviaword3").classList.add("activelink");
+}
+if(pageName ==="Lesson4.html"){
+    document.querySelector(".barword").classList.add("activelink");
+}
+if(pageName ==="Lesson4.html"){
+    document.querySelector(".divaword4").classList.add("activelink");
+}
+if(pageName ==="Lesson5.html"){
+    document.querySelector(".barword").classList.add("activelink");
+}
+if(pageName ==="Lesson5.html"){
+    document.querySelector(".divaword5").classList.add("activelink");
+}
+if(pageName ==="Lesson6.html"){
+    document.querySelector(".barword").classList.add("activelink");
+}
+if(pageName ==="Lesson6.html"){
+    document.querySelector(".divaword6").classList.add("activelink");
+}
 //book1 english
 if(pageName ==="Eng_Lesson1.html"){
     document.querySelector(".bareng").classList.add("activelink");
 }
-alert(pageName);
-alert(pathName);
+if(pageName ==="Eng_Lesson1.html"){
+    document.querySelector(".divaone").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson2.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson2.html"){
+    document.querySelector(".divatwo").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson3.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson3.html"){
+    document.querySelector(".divathree").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson4.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson4.html"){
+    document.querySelector(".divafour").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson5.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson5.html"){
+    document.querySelector(".divafive").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson6.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson6.html"){
+    document.querySelector(".divasix").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson7.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson7.html"){
+    document.querySelector(".divaseven").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson8.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson8.html"){
+    document.querySelector(".divaeight").classList.add("activelink");
+}
+
+    if(pageName ==="Eng_Lesson9.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson9.html"){
+    document.querySelector(".divanine").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson16.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson16.html"){
+    document.querySelector(".divasixteen").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson17.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson17.html"){
+    document.querySelector(".divaseventeen").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson18.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson18.html"){
+    document.querySelector(".divaeighteen").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson19.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson19.html"){
+    document.querySelector(".divanineteen").classList.add("activelink");
+}
+    if(pageName ==="Eng_Lesson20.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson20.html"){
+    document.querySelector(".divatwenty").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson21.html"){
+    document.querySelector(".bareng").classList.add("activelink");
+}
+if(pageName ==="Eng_Lesson21.html"){
+    document.querySelector(".divatwentyone").classList.add("activelink");
+}
+
+
